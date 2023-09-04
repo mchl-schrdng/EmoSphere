28fe4692a69e9f67531741563ae36e14481bce00
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-09-04 16:56:58 +0200

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -4,8 +4,21 @@ import supabase
 # Initialize Supabase client
 url = st.secrets["SUPABASE_URL"]
 api_key = st.secrets["SUPABASE_API_KEY"]
-supabase_client = supabase.create_client(url, api_key)
+supabase_client = create_client(url, api_key)
 
 def insert_word(word: str):
-    # Insert word into the 'words' table
-    supabase.table('user_emotions').insert({"word": word}).execute()
+    """Insert a new word into the user_emotions table."""
+    table = "user_emotions"
+    data = {"word": word}
+    response = supabase_client.table(table).insert(data).execute()
+    return response
+
+def retrieve_words(time_range):
+    """Retrieve words from the user_emotions table based on a selected time range."""
+    table = "user_emotions"
+    start_time, end_time = time_range
+    query = (
+        f"SELECT word FROM {table} WHERE created_at >= '{start_time}' AND created_at <= '{end_time}'"
+    )
+    response = supabase_client.raw(query).execute()
+    return response
