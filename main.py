fc244c804622e7ee6f6f82fa0ca3ccb093292794
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-09-04 16:40:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import streamlit as st
+import base64
 from utils.database import insert_word
 
+def get_image_base64(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Style
+st.markdown("""
+    <style>
+    .centered {
+        text-align: center;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Get the Base64 string of the image
+img_base64 = get_image_base64("assets/images/logo.png")
+
 # Display centered logo
-st.write("<div style='text-align: center;'><br>", unsafe_allow_html=True)
-st.image("images/logo.png", width=200)
-st.write("</div>", unsafe_allow_html=True)
+st.markdown(f'<p class="centered"><img src="data:image/png;base64,{img_base64}" style="max-width:200px; height:auto;"></p>', unsafe_allow_html=True)
 
 # Main function for Streamlit app
 def main():
