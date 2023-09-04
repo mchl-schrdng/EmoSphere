0f2cfaf0e0fd3e5ecce2cb3e3d0c9d3b965f287b
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-09-04 15:20:04 +0200

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,9 +1,9 @@
-from supabase_py import create_client, Client
+import supabase
 
 # Initialize Supabase client
-url: str = "your-supabase-url"
-api_key: str = "your-supabase-api-key"
-supabase: Client = create_client(url, api_key)
+url = st.secrets["SUPABASE_URL"]
+api_key = st.secrets["SUPABASE_API_KEY"]
+supabase_client = supabase.create_client(url, api_key)
 
 def insert_word(word: str):
     # Insert word into the 'words' table
