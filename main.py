cf6509e0608c998bf75ec6e62ad2c36b83abfc38
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-09-05 10:12:24 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import streamlit as st
 import polars as pl
+import base64
+
 from utils.database import insert_word, retrieve_words
 
+def get_image_base64(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Style
+st.markdown("""
+    <style>
+    .centered {
+        text-align: center;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Get the Base64 string of the image
+img_base64 = get_image_base64("images/logo.png")
+
+# Display centered logo
+st.markdown(f'<p class="centered"><img src="data:image/png;base64,{img_base64}" style="max-width:200px; height:auto;"></p>', unsafe_allow_html=True)
+
+# Display centered title
+st.markdown('<h1 class="centered">EmoSphere</h1>', unsafe_allow_html=True)
+
+# Main function for Streamlit app
 def main():
     st.title("Emotional Landscape")
 
